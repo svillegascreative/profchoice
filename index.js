d73a76de0f18d577d90f5e6e639d9dc512662d8d
sylvia villegas <sylvia@svillegas.com>
2019-03-06 16:35:29 -0500

    Get some values using cheerio

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 // adapted from https://medium.freecodecamp.org/the-ultimate-guide-to-web-scraping-with-node-js-daa2027dcd3
 
 const rp = require('request-promise-native');
+const $ = require('cheerio');
 const url = 'https://profchoice.com/search.html';
 
 rp(url)
   .then(function (html) {
-    console.log(html);
+    let items = $('.wsm-cat-list-item', html);
+    // console.log( items.length );
+    
+    let itemLinks = $('.wsm-cat-title a', items);
+    // Array.from(itemLinks).forEach( function (link) {
+    //   console.log(link);
+    // })
+    
+    let itemNames = $('.wsm-cat-title a', items).text();
+    console.log(itemNames);
+    
   })
   .catch(function (err) {
     console.log(err);
